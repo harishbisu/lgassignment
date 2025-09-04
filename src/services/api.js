642cf1bc6cbea872e6703ea37fdcb36f68a2cf58
harishbisu <90022465+harishbisu@users.noreply.github.com>
2025-09-04 08:29:25 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:3000";
+const API_BASE = "https://lgassign-backend.onrender.com/";
 
 export const trackEmail = async (subject) => {
   const res = await axios.get(`${API_BASE}/emails/${subject}`);
